8c88f32b0c891ce9f55aa9fe8b512b4ae229cc8e
Daniil <daniilbelkin8@gmail.com>
2023-06-11 20:56:36 +0800

    add blog

---
+++ src/routes/blog/routes.js
@@ -0,0 +1,12 @@
+export const routes = [
+    {
+        id: 1,
+        title: "My first blog post",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate"
+    },
+    {
+        id: 2,
+        title: "My second blog post",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate"
+    }
+];
